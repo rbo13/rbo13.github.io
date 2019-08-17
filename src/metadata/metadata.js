22df4296996151fff65f14bb2f4c56fde64f93d8
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-08-17 13:15:36 +0800

    initial commit
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

---
+++ src/metadata/metadata.js
@@ -0,0 +1,65 @@
+module.exports = {
+  name: 'Richard Burk A. Orofeo I',
+  title: 'Software Developer',
+	baseUrl: 'https://github.com/rbo13',
+	mobile: '+63966 411 4873',
+  facts: {
+    'Residence': 'Cebu City, Philippines',
+		'Homepage': `<a href="https://rbo13.keybase.pub">
+									rbo13
+                </a>`,
+    'Email': '<a href="mailto:rborofeo@gmail.com">rborofeo@gmail.com</a>'
+  },
+  skills: [
+    ['Go', '++++'],
+    ['NodeJS', '++++'],
+    ['JavaScript', '++++'],
+    ['GraphQL', '+++'],
+    ['Python', '+++'],
+    ['MySQL', '++++'],
+    ['Git', '++++'],
+    ['Docker', '+++'],
+    ['Kubernetes', '+++'],
+    ['AWS', '+++'],
+    ['GCP', '+++'],
+		['CI/CD', '+++'],
+  ],
+  plays: [
+    {
+      title: 'Senior Software Engineer',
+			company: 'A-FIS Lab Ph. Inc',
+      written: 'August 2018 - Present',
+      badges: ['Go', 'MySQL', 'CI/CD', 'AWS', 'Deployment'],
+      contents: `
+Lead a small team of developers to work with our In-house, and Client Projects. We design and develop scalable backend architecture. Also, I am the point of contact in regards with our technical specifications. Configures our CI/CD pipeline for the deployment of our web applications using AWS running on ECS.
+`
+    },
+    {
+      title: 'Junior Software Engineer',
+			company: 'A-FIS Lab Ph. Inc',
+      written: 'August 2017 - August 2018',
+      badges: ['Go', 'MySQL', 'Docker', 'JavaScript', 'ReactJS'],
+      contents: `
+Applied clean code practices and design patterns to develop readable, maintainable and scalable code for a web application. Improved existing code using SOLID design principles. Worked on creating a reusable internal library for our different projects.
+`
+    },
+    {
+      title: 'Junior Software Developer',
+			company: 'Coderepubliq',
+      written: 'June 2016 - Jan 2017',
+      badges: ['JavaScript', 'NodeJS', 'AngularJS', 'MEAN'],
+      contents: `
+Works as a backend developer. I design and develop RESTful APIs with NodeJS. Assisted mobile and frontend teams in solving different issues.
+`
+    }
+  ],
+  experience: [
+    {
+      header: 'Freelance Developer',
+      skills: ['JavaScript', 'NodeJS'],
+      contents: `
+Works as a remote backend developer using NodeJS. Also, created a tweet bot to aggregate news from different sources. Maintains performance of existing client websites.
+`
+    },
+  ]
+};
